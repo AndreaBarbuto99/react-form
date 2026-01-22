fbdb668a07a6b8eee898161fa244bf76e14ee0b1
Andrea Barbuto <barbutoandrea99@gmail.com>
2026-01-22 18:03:40 +0100

    feat: added script to add a blog

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -9,20 +9,41 @@ const initialBlogList = [
 
 export default function BlogList() {
 
-
-
+    const [blogs, setBlogs] = useState(initialBlogList);
+    const [newBlog, setNewBlog] = useState("");
 
     // return principale della funzione e component BlogList //
+    function addBlog(e) {
+        e.preventDefault();
+        const newBlogList = [...blogs, newBlog]
+        setBlogs(newBlogList);
+        setNewBlog("");
+
+    }
+
+
+
     return (
+
         <div>
             <h1>Blog</h1>
+
             <div className="container mt-5">
-                {initialBlogList.map((blog) => {
-                    return <p className="border border-2 border-primary rounded p-2 bg-light">{blog}</p>
+                {blogs.map((blog, index) => {
+                    return <p key={index} className="border border-2 border-primary rounded p-2 bg-light">{blog}</p>
                 })}
-
-
             </div>
+            <form onSubmit={addBlog}>
+                <div className="container input-group">
+                    <input type="text"
+                        className="form-control rounded"
+                        placeholder="Aggiungi un blog"
+                        value={newBlog}
+                        onChange={element => { setNewBlog(element.target.value) }} />
+                    <button className="btn btn-primary">Aggiungi</button>
+                </div>
+            </form>
         </div>
+
     )
 }
